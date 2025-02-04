9621b1fa8a6285aa0a3b13a1929e4f27efa1eb57
Sullivan Delory <sudelory@z2r8p6.42lyon.fr>
2025-02-04 20:02:33 +0100

    first push for push_swap

---
+++ check_and_fill_pile.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_and_fill_pile.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudelory <sudelory@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 17:14:29 by sudelory          #+#    #+#             */
+/*   Updated: 2025/02/04 17:45:24 by sudelory         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+// Function to check if the stack is empty
+int	check_if_empty(int size)
+{
+	return (size == 0);
+}
+
+// Function to check if the stack is full
+int	check_if_full(int capacity, int size)
+{
+	return (size == capacity);
+}
+
+// Function to add an element to the top of the stack
+int	fill_top_of_stack(int top, int *array)
+{
+
+}
---
+++ push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudelory <sudelory@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 17:32:21 by sudelory          #+#    #+#             */
+/*   Updated: 2025/02/04 17:27:55 by sudelory         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+// Structure to initialize Stack A
+typedef struct init_stack
+{
+	int	*array;
+	int	top;
+	int	size;
+	int	max_capacity;
+}	t_init_stack;
+
+#endif
